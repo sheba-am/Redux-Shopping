5601bc4e7d9ae20eb944ab4be63318af3753b8f2
sheba-am <saba.ameri1378@gmail.com>
2025-01-08 22:36:16 +0330

    when switching the single product don't show prev pr before the new pr

--- redux-shopping/src/containers/ProductDetail.js
+++ redux-shopping/src/containers/ProductDetail.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, {useEffect} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { selectedProduct } from "../redux/actions/productActions";
+import { removeSelectedProduct, selectedProduct } from "../redux/actions/productActions";
 
 const ProductDetail = () => {
     const product = useSelector((state) => state.product);
@@ -21,6 +21,9 @@ const ProductDetail = () => {
     };
     useEffect(() => {
         if (productId && productId !== "") fetchProductDetail();
+        return() => {
+            dispatch(removeSelectedProduct());
+        }; 
     }, [productId])
     return (
         <div className="">
--- redux-shopping/src/redux/reducers/productReducer.js
+++ redux-shopping/src/redux/reducers/productReducer.js
@@ -20,7 +20,8 @@ export const selectedProductReducer = (state={}, {type,payload}) => {
     switch (type) {
         case ActionTypes.SELECTED_PRODUCTS:
             return {...state,...payload}
-
+        case ActionTypes.REMOVE_SELECTED_PRODUCTS:
+            return {}
     
         default:
             return state;
